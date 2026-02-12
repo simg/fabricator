720de46a57cb3f35b595f29622eb7f120348b84e
Simon Gardner <slgard@gmail.com>
2026-02-12 18:49:11 +0000

    add github deploy

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import reactPlugin from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [reactPlugin()],
+  base: command === "build" ? "/fabricator/" : "/",
   worker: {
     format: "es",
   },
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
   server: {
     port: 4444,
   },
-});
+}));
